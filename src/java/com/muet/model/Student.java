cb1b7f2392edd427c31ad1ffee7b72c42a8ead26
jawadzour <jawadzour786@gmail.com>
2020-08-06 14:39:46 +0500

    Srudent modal added

--- src/java/com/muet/model/Student.java
+++ src/java/com/muet/model/Student.java
@@ -5,6 +5,8 @@
  */
 package com.muet.model;
 
+import java.util.Date;
+
 /**
  *
  * @author 92310
@@ -13,7 +15,223 @@ public class Student{
     private int studentId;
     private String fullName;
     private String rollNumber;
+    private String fathersName;
+    private String legalId;
+    private String legalIdNo;
+    private int nationality;
+    private String placeOfIssueOfLegalId;
+    private Date dateOfIssueOfLegalId;
+    private String mobile;
+    private Date dob;
+    private String email;
+    private String domicile;
+    private String countryOfBirth;
+    private String bloodGroup;
+    private String religion;
+    private String address;
+    private String currentAddress;
 
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public void setFathersName(String fathersName) {
+        this.fathersName = fathersName;
+    }
+
+    public String getLegalId() {
+        return legalId;
+    }
+
+    public void setLegalId(String legalId) {
+        this.legalId = legalId;
+    }
+
+    public String getLegalIdNo() {
+        return legalIdNo;
+    }
+
+    public void setLegalIdNo(String legalIdNo) {
+        this.legalIdNo = legalIdNo;
+    }
+
+    public int getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(int nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPlaceOfIssueOfLegalId() {
+        return placeOfIssueOfLegalId;
+    }
+
+    public void setPlaceOfIssueOfLegalId(String placeOfIssueOfLegalId) {
+        this.placeOfIssueOfLegalId = placeOfIssueOfLegalId;
+    }
+
+    public Date getDateOfIssueOfLegalId() {
+        return dateOfIssueOfLegalId;
+    }
+
+    public void setDateOfIssueOfLegalId(Date dateOfIssueOfLegalId) {
+        this.dateOfIssueOfLegalId = dateOfIssueOfLegalId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDomicile() {
+        return domicile;
+    }
+
+    public void setDomicile(String domicile) {
+        this.domicile = domicile;
+    }
+
+    public String getCountryOfBirth() {
+        return countryOfBirth;
+    }
+
+    public void setCountryOfBirth(String countryOfBirth) {
+        this.countryOfBirth = countryOfBirth;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCurrentAddress() {
+        return currentAddress;
+    }
+
+    public void setCurrentAddress(String currentAddress) {
+        this.currentAddress = currentAddress;
+    }
+
+    public int getSemeter() {
+        return semeter;
+    }
+
+    public void setSemeter(int semeter) {
+        this.semeter = semeter;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public Date getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(Date admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public int getSupervisorId() {
+        return supervisorId;
+    }
+
+    public void setSupervisorId(int supervisorId) {
+        this.supervisorId = supervisorId;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getFieldProgram() {
+        return fieldProgram;
+    }
+
+    public void setFieldProgram(String fieldProgram) {
+        this.fieldProgram = fieldProgram;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getTiming() {
+        return timing;
+    }
+
+    public void setTiming(String timing) {
+        this.timing = timing;
+    }
+    private int semeter ;
+    private int departmentId;
+    private String batch;
+    private Date admissionDate;
+    private int supervisorId;
+    private String program;
+    private String fieldProgram;
+    private String shift;
+    private String timing;
+    
     public int getStudentId() {
         return studentId;
     }
